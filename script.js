559dc3e4a953427cc47939b03b468e2203e85379
Shayanthan Ravindran <shayan_ravi@hotmail.com>
2023-02-23 10:19:09 +0100

    code changed

--- script.js
+++ script.js
@@ -1,35 +1,45 @@
-const form = document.querySelector("form");
-eField = form.querySelector(".email"),
-    eInput = eField.querySelector("input"),
-    pField = form.querySelector(".password"),
-    pInput = pField.querySelector("input");
+// Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-app.js";
+  import { getAuth,createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyCIFTGTaVFt45FoyMbTYmGDlkbzE-yqKAU",
+    authDomain: "signup-form-b852a.firebaseapp.com",
+    projectId: "signup-form-b852a",
+    storageBucket: "signup-form-b852a.appspot.com",
+    messagingSenderId: "624575145056",
+    appId: "1:624575145056:web:88a771e81a5d307cc48f05"
+  };
 
-    function checkEmail() {
-        let pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
-        if (!eInput.value.match(pattern)) {
-            eField.classList.add("error");
-            eField.classList.remove("valid");
-            let errorTxt = eField.querySelector(".error-txt");
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const auth =  getAuth()
 
-            (eInput.value != "") ? errorTxt.innerText = "Enter a valid email address": errorTxt.innerText = "Email can't be blank";
-        } else {
-            eField.classList.remove("error");
-            eField.classList.add("valid");
-        }
-    }
+  //getting All the Objects of html
+  var firstname = document.getElementById("firstname")
+  var lastname = document.getElementById("lastname")
+  var email = document.getElementById("email")
+  var password = document.getElementById("password")
 
-    function checkPass() {
-        if (pInput.value == "") {
-            pField.classList.add("error");
-            pField.classList.remove("valid");
-        } else {
-            pField.classList.remove("error");
-            pField.classList.add("valid");
-        }
-    }
-
-    if (!eField.classList.contains("error") && !pField.classList.contains("error")) {
-        window.location.href = form.getAttribute("action");
-    }
-   
+  //making an function for staring data
+  window.signup = function(e){
+    e.preventDefault();
+    var obj = {
+        firstname:firstname.value,
+        lastname:lastname.value,
+        email:email.value,
+        password:password.value
+    };
+    createUserWithEmailAndPassword(auth, obj.email, obj.password)
+    .then(function(success){
+        window.replace('login.html')
+        alert("Signup Successfully")
+    })
+    .catch(function(err){
+        alert("error in" + err)
+    });
+    console.log(obj);
+  };
